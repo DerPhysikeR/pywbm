f4d9ad191a46119c565b8df94e404c61ef07288d
Paul Reiter <reiter.paul@gmail.com>
2017-05-25 15:06:22 +0200

    Completed `Subdomain` class with code from pywbm.py script

--- pywbm/subdomain.py
+++ pywbm/subdomain.py
@@ -3,7 +3,10 @@
 2017-05-14 18:35:02
 @author: Paul Reiter
 """
+from collections import OrderedDict
+from functools import partial
 import numpy as np
+from .wavefunctions import Wavefunctions, line_integral
 
 
 class Subdomain():
@@ -16,6 +19,7 @@ class Subdomain():
         self.elements = np.array(elements)
         self.lx = np.max(self.nodes[:, 0]) - np.min(self.nodes[:, 0])
         self.ly = np.max(self.nodes[:, 1]) - np.min(self.nodes[:, 1])
+        self.solutions = OrderedDict()
 
     @property
     def normals(self):
@@ -27,18 +31,47 @@ class Subdomain():
             normals[i, 1] = (p1[0]-p0[0]) / length
         return normals
 
-    def get_subdomain_matrix(self, z, k):
-        pass
+    def solve(self, z, k, n, vn):
+        if (z, k, n, vn) not in self.solutions:
+            wv = Wavefunctions(k, self.lx, self.ly)
+            a = self.a_ij(z, k, wv.phiw, wv.grad_phiw, n)
+            rhs = self.rhs_i(z, k, wv.phiw, vn, n)
+        self.solutions[(z, k, n, vn)] = np.linalg.solve(a, rhs)
 
-    def get_subdomain_rhs(self, z, k):
-        pass
+    def get_av(self, pw, gpw, z, k):
+        def av(nx, ny, x, y):
+            return 1j/(z*k)*pw(x, y)*gpw(nx, ny, x, y)
+        return av
 
-    def add_source(self):
-        pass
+    def get_rhs(self, pw, vn):
+        def fv(nx, ny, x, y):
+            return pw(x, y)*vn(x, y)
+        return fv
 
+    def a_ij(self, z, k, phiw, grad_phiw, n):
+        a = np.zeros((len(phiw), len(phiw)), dtype=complex)
+        for i, pw in enumerate(phiw):
+            for j, gpw in enumerate(grad_phiw):
+                for normal, element in zip(self.normals, self.elements):
+                    p0, p1 = self.nodes[element[0]], self.nodes[element[1]]
+                    fun = partial(self.get_av(pw, gpw, z, k),
+                                  normal[0], normal[1])
+                    a[i, j] += line_integral(fun, p0, p1, n)
+        return a
 
-# class BoundedSubdomain(Subdomain):
-#     pass
+    def rhs_i(self, z, k, phiw, vn, n):
+        rhs = np.zeros(len(phiw), dtype=complex)
+        for i, pw in enumerate(phiw):
+            for normal, element in zip(self.normals, self.elements):
+                p0, p1 = self.nodes[element[0]], self.nodes[element[1]]
+                fun = partial(self.get_rhs(pw, vn), normal[0], normal[1])
+                rhs[i] += line_integral(fun, p0, p1, n)
+        return rhs
 
-# class UnboundedSubdomain(Subdomain):
-#     pass
+    def field_solution(self, x, y, z, k, n, vn):
+        if (z, k, n, vn) not in self.solutions:
+            self.solve(z, k, n, vn)
+
+        pw = self.solutions[(z, k, n, vn)]
+        wv = Wavefunctions(k, self.lx, self.ly)
+        return sum(p[0]*p[1](x, y) for p in zip(pw, wv.phiw))
