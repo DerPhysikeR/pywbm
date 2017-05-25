a281f056a27e99e9efa3fc80adf4eb4cb3107b9a
Paul Reiter <reiter.paul@gmail.com>
2017-05-25 20:56:36 +0200

    Enable different kinds of boundary conditions

--- pywbm/subdomain.py
+++ pywbm/subdomain.py
@@ -11,16 +11,43 @@ from .wavefunctions import Wavefunctions, line_integral
 
 class Subdomain():
 
-    def __init__(self, nodes, elements, bounded=True, cache_length=np.inf):
+    def __init__(self, nodes, elements, kinds, functions, sources=None,
+                 bounded=True, cache_length=np.inf):
+        """Creates a single `Subdomain` for the wave based method.
+
+        Parameters
+        ----------
+        nodes : array-like
+            List or array of coordinate tuples
+        elements : array-like
+            List or array of node-number tuples
+        kinds : array-like
+            List or array of characters. Only 'v', 'z', 'p', 'i' are valid.
+            Marking velocity, impedance, pressure and interface boundary
+            conditions.
+        functions : list
+            List of functions f(x, y) for the quantities given by `kinds`
+        sources : list
+            List of source functions p(x, y) inside domain
+        bounded : boolean
+            Optional, marks boundary as bounded as opposed to open
+            Default is True, which means bounded.
+        cache_length : integer
+            Optional, number of solutions saved if `solve` is called multiple
+            times with different parameters
+        """
         if max([max(ele) for ele in elements]) > len(nodes)-1:
             raise ValueError('Element references non-existing node!')
         self.bounded = bounded
         self.nodes = np.array(nodes)
         self.elements = np.array(elements)
+        self.kinds = kinds
+        self.functions = functions
         self.lx = np.max(self.nodes[:, 0]) - np.min(self.nodes[:, 0])
         self.ly = np.max(self.nodes[:, 1]) - np.min(self.nodes[:, 1])
         self.solutions = OrderedDict()
         self.cache_length = cache_length
+        self.sources = sources
 
     @property
     def normals(self):
@@ -41,34 +68,67 @@ class Subdomain():
             self.solutions.popitem(last=False)
         self.solutions[(z, k, n, vn)] = np.linalg.solve(a, rhs)
 
-    def get_av(self, pw, gpw, z, k):
-        def av(normal, x, y):
-            return 1j/(z*k)*pw(x, y)*gpw(normal[0], normal[1], x, y)
-        return av
+    def get_a(self, pwt, gpwt, pw, gpw, z, k, kind, fun):
+        if kind == 'v':
+            def afun(n, x, y):
+                return 1j/(z*k)*pwt(x, y)*gpw(n[0], n[1], x, y)
+        elif kind == 'z':
+            def afun(n, x, y):
+                return (1j/(z*k)*pwt(x, y)*gpw(n[0], n[1], x, y) -
+                        1/fun(x, y)*pwt(x, y)*pw(x, y))
+        elif kind == 'p':
+            def afun(n, x, y):
+                return -1j/(z*k)*gpwt(n[0], n[1], x, y)*pw(x, y)
+        elif kind == 'i':
+            def afun(n, x, y):
+                return (1j/(z*k)*pwt(x, y)*gpw(n[0], n[1], x, y) -
+                        1/fun(x, y)*pwt(x, y)*pw(x, y))
+        else:
+            raise ValueError('Only kinds v, z, p and i are allowed for'
+                             ' element!')
+        return afun
 
-    def get_rhs(self, pw, vn):
-        def fv(normal, x, y):
-            return pw(x, y)*vn(x, y)
-        return fv
+    def get_rhs(self, pwt, gpwt, z, k, kind, fun):
+        if kind == 'v':
+            def ffun(n, x, y):
+                return pwt(x, y)*fun(x, y)
+        elif kind == 'z':
+            def ffun(n, x, y):
+                return 0
+        elif kind == 'p':
+            def ffun(n, x, y):
+                return 0
+        elif kind == 'i':
+            def ffun(n, x, y):
+                return 0
+        else:
+            raise ValueError('Only kinds v, z, p and i are allowed for'
+                             ' element!')
+        return ffun
 
     def a_ij(self, z, k, n):
         wv = Wavefunctions(k, self.lx, self.ly)
         a = np.zeros((len(wv.phiw), len(wv.phiw)), dtype=complex)
-        for i, pw in enumerate(wv.phiw):
-            for j, gpw in enumerate(wv.grad_phiw):
-                for normal, element in zip(self.normals, self.elements):
+        # pwt ... t is for transposed
+        for i, (pwt, gpwt) in enumerate(zip(wv.phiw, wv.grad_phiw)):
+            for j, (pw, gpw) in enumerate(zip(wv.phiw, wv.grad_phiw)):
+                for normal, element, kind, fun in zip(
+                  self.normals, self.elements, self.kinds, self.functions):
                     p0, p1 = self.nodes[element[0]], self.nodes[element[1]]
-                    fun = partial(self.get_av(pw, gpw, z, k), normal)
+                    fun = partial(self.get_a(pwt, gpwt, pw, gpw, z, k, kind,
+                                             fun), normal)
                     a[i, j] += line_integral(fun, p0, p1, n)
         return a
 
     def rhs_i(self, z, k, vn, n):
         wv = Wavefunctions(k, self.lx, self.ly)
         rhs = np.zeros(len(wv.phiw), dtype=complex)
-        for i, pw in enumerate(wv.phiw):
-            for normal, element in zip(self.normals, self.elements):
+        # pwt ... t is for transposed
+        for i, (pwt, gpwt) in enumerate(zip(wv.phiw, wv.grad_phiw)):
+            for normal, element, kind, fun in zip(
+              self.normals, self.elements, self.kinds, self.functions):
                 p0, p1 = self.nodes[element[0]], self.nodes[element[1]]
-                fun = partial(self.get_rhs(pw, vn), normal)
+                fun = partial(self.get_rhs(pwt, gpwt, z, k, kind, fun), normal)
                 rhs[i] += line_integral(fun, p0, p1, n)
         return rhs
 
