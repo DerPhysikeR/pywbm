f82127d30734e23dc2ad1f5594d4ddae7e5c662e
Paul Reiter <reiter.paul@gmail.com>
2017-08-31 16:57:26 +0200

    TST add test for interface bc

--- pywbm/tests/test_subdomain.py
+++ pywbm/tests/test_subdomain.py
@@ -112,6 +112,34 @@ def test_impedance_bc(frequency):
                     np.array(solution))
 
 
+@pytest.mark.parametrize('frequency', [100, 300, 800, 1000])
+def test_interface_bc(frequency):
+    # interface condition should act identical to impedance condition if no
+    # other domain exists
+    z = 1.205*343.4
+    k = 2*np.pi*frequency/343.4
+    lx, ly, n = 2, 1, 20
+    nodes = [(0, 0), (lx, 0), (lx, ly), (0, ly)]
+    elements = [(0, 1), (1, 2), (2, 3), (3, 0)]
+    kinds = ['v', 'p', 'v', 'i']
+    functions = [lambda *args: 0, lambda *args: 1,
+                 lambda *args: 0, lambda *args: 2*z]
+    sd = Subdomain(nodes, elements, kinds, functions)
+
+    field_points = [(x, ly/2) for x in np.linspace(0, lx, 10)]
+    solution = [sd.field_solution(x, y, z, k, n) for x, y in field_points]
+
+    # for impedance Z = 2*rho*c
+    g = 1/3
+    b = 1/(g*np.exp(1j*k*lx) + np.exp(-1j*k*lx))
+    a = (1-b*np.exp(-1j*k*lx))/np.exp(1j*k*lx)
+    reference_solution = [reference_pressure(a, b, k, x)
+                          for x, y in field_points]
+
+    assert_allclose(np.array(reference_solution),
+                    np.array(solution))
+
+
 @pytest.mark.parametrize('x, y, inside', [
     (.5, .5, True),
     (1, 1, True),
